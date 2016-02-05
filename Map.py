21a2fa5faafb2633ebc969e9090ae519f5d622b8
Alicia White <awhite40@gatech.edu>
2016-02-05 11:26:11 -0500

    1 to 0

--- Map.py
+++ Map.py
@@ -16,7 +16,7 @@ M_cw1_E_y = M_cw1_S_y + 42
 
 print M_cw1_E_x, M_cw1_E_y
 #M_cw1_E = (M_cw1_S[0] + 4, M_cw1_S[1] + 42)
-map[M_cw1_S_x:M_cw1_E_x, M_cw1_S_y:M_cw1_E_y] = 1
+map[M_cw1_S_x:M_cw1_E_x, M_cw1_S_y:M_cw1_E_y] = 0
 
 # 1st Sidewalk
 
@@ -26,7 +26,7 @@ M_sw1_E_x = M_sw1_S_x + 5
 M_sw1_E_y = M_sw1_S_y + 176
 
 print M_sw1_S_x, M_sw1_S_y, M_sw1_E_x, M_sw1_E_y
-map[M_sw1_S_x:M_sw1_E_x, M_sw1_S_y:M_sw1_E_y] = 1
+map[M_sw1_S_x:M_sw1_E_x, M_sw1_S_y:M_sw1_E_y] = 0
 
 # 2nd Crosswalk
 M_cw2_S_x = M_sw1_S_x
@@ -34,14 +34,14 @@ M_cw2_S_y = M_sw1_E_y
 M_cw2_E_x = M_cw2_S_x + 4
 M_cw2_E_y = M_cw2_S_y + 19
 
-map[M_cw2_S_x:M_cw2_E_x, M_cw2_S_y:M_cw2_E_y] = 1
+map[M_cw2_S_x:M_cw2_E_x, M_cw2_S_y:M_cw2_E_y] = 0
 
 # 2nd Sidewalk part 1 - up to Varsity
 M_sw2_S_x = M_cw2_S_x - 1
 M_sw2_S_y = M_cw2_E_y
 M_sw2_E_x = M_sw2_S_x + 6
 M_sw2_E_y = M_sw2_S_y + 195
-map[M_sw2_S_x:M_sw2_E_x, M_sw2_S_y:M_sw2_E_y] = 1
+map[M_sw2_S_x:M_sw2_E_x, M_sw2_S_y:M_sw2_E_y] = 0
 
 
 # 2nd Sidewalk part 2
@@ -50,7 +50,7 @@ M_sw2b_S_x = M_sw2_S_x
 M_sw2b_S_y = M_sw2_E_y
 M_sw2b_E_x = M_sw2b_S_x + 6
 M_sw2b_E_y = M_sw2b_S_y + 67
-map[M_sw2b_S_x:M_sw2b_E_x, M_sw2b_S_y:M_sw2b_E_y] = 1
+map[M_sw2b_S_x:M_sw2b_E_x, M_sw2b_S_y:M_sw2b_E_y] = 0
 
 # 3rd Crosswalk
 M_cw3_S_x = M_sw2b_S_x + 1
@@ -58,14 +58,14 @@ M_cw3_S_y = M_sw2b_E_y
 M_cw3_E_x = M_cw3_S_x + 4
 M_cw3_E_y = M_cw3_S_y + 15
 
-map[M_cw3_S_x:M_cw3_E_x, M_cw3_S_y:M_cw3_E_y] = 1
+map[M_cw3_S_x:M_cw3_E_x, M_cw3_S_y:M_cw3_E_y] = 0
 
 # 3rd Sidewalk
 M_sw3_S_x = M_cw3_S_x - 1
 M_sw3_S_y = M_cw3_E_y
 M_sw3_E_x = M_sw3_S_x + 6
 M_sw3_E_y = M_sw3_S_y + 217
-map[M_sw3_S_x:M_sw3_E_x, M_sw3_S_y:M_sw3_E_y] = 1
+map[M_sw3_S_x:M_sw3_E_x, M_sw3_S_y:M_sw3_E_y] = 0
 
 # 4th Crosswalk
 M_cw4_S_x = M_sw3_S_x + 1
@@ -73,7 +73,7 @@ M_cw4_S_y = M_sw3_E_y
 M_cw4_E_x = M_cw4_S_x + 4
 M_cw4_E_y = M_cw4_S_y + 27
 
-map[M_cw4_S_x:M_cw4_E_x, M_cw4_S_y:M_cw4_E_y] = 1
+map[M_cw4_S_x:M_cw4_E_x, M_cw4_S_y:M_cw4_E_y] = 0
 
 
 
@@ -85,7 +85,7 @@ N_cw1_S_y = 800
 N_cw1_E_x = M_cw1_S_x + 32
 N_cw1_E_y = M_cw1_S_y + 4
 
-map[N_cw1_S_x:N_cw1_E_x, N_cw1_S_y:N_cw1_E_y] = 1
+map[N_cw1_S_x:N_cw1_E_x, N_cw1_S_y:N_cw1_E_y] = 0
 
 
 # 2nd Crosswalk - 42 spaces East
@@ -94,7 +94,7 @@ N_cw2_S_y = N_cw1_S_y
 N_cw2_E_x = N_cw2_S_x + 4
 N_cw2_E_y = N_cw2_S_y + 42
 print N_cw2_E_x, N_cw2_E_y
-map[N_cw2_S_x:N_cw2_E_x, N_cw2_S_y:N_cw2_E_y] = 1
+map[N_cw2_S_x:N_cw2_E_x, N_cw2_S_y:N_cw2_E_y] = 0
 
 
 # Sidewalk 185 spaces south
@@ -102,28 +102,28 @@ N_sw1_S_x = N_cw2_S_x
 N_sw1_S_y = N_cw2_E_y
 N_sw1_E_x = N_sw1_S_x + 189
 N_sw1_E_y = N_sw1_S_y + 3
-map[N_sw1_S_x:N_sw1_E_x, N_sw1_S_y:N_sw1_E_y] = 1
+map[N_sw1_S_x:N_sw1_E_x, N_sw1_S_y:N_sw1_E_y] = 0
 
 # Drive to gate - 60 spaces east
 N_sw2_S_x = N_sw1_E_x
 N_sw2_S_y = N_sw1_S_y
 N_sw2_E_x = N_sw2_S_x + 4
 N_sw2_E_y = N_sw2_S_y + 60
-map[N_sw2_S_x:N_sw2_E_x, N_sw2_S_y:N_sw2_E_y] = 1
+map[N_sw2_S_x:N_sw2_E_x, N_sw2_S_y:N_sw2_E_y] = 0
 
 # Other side of street to Marta
 # sidewalk 1
-map[836:841,842:1018] = 1
+map[836:841,842:1018] = 0
 # Crosswalk 2
-map[836:840,1018:1037] = 1
+map[836:840,1018:1037] = 0
 # Sidewalk 2
-map[835:841,1037:1299] = 1
+map[835:841,1037:1299] = 0
 #Crosswalk 3
-map[836:840,1299:1314] = 1
+map[836:840,1299:1314] = 0
 #Sidewalk 3
-map[835:841,1314:1531] = 1
+map[835:841,1314:1531] = 0
 #Crosswalk 4
-map[836:840,1531:1558] = 1
+map[836:840,1531:1558] = 0
 
 
 
