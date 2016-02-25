88f230dd6e8b20a8bb13fd1fc8fb9c33d57cb02b
mlihc15 <mlihc15@gmail.com>
2016-02-25 17:10:47 -0500

    Final Simulation

--- pedestrian.py
+++ pedestrian.py
@@ -1,9 +1,13 @@
 import numpy as np
 import random
-#import math
-import Map
+import math
+import Map as map
+import queue
 from matplotlib import pyplot as plt
 from collections import namedtuple
+
+cw_close_period = 240
+cw_close_time = 60
 
 
 MartaFF = np.loadtxt('Marta_floor_field.txt')
@@ -33,26 +37,6 @@ def findFF(destination):
 
 
 personStruct = namedtuple("personStruct", "index gate destin x y status")
-active_list = []
-m = personStruct(0, 'C', 'NorthAve', 800, 800, 1)
-active_list.append(m)
-Map.map[800, 800] = 1
-m = personStruct(1, 'C', 'Marta', 801, 800, 1)
-active_list.append(m)
-Map.map[801, 800] = 1
-m = personStruct(2, 'C', 'Peters_Parking_Deck', 802, 800, 1)
-active_list.append(m)
-Map.map[802, 800] = 1
-m = personStruct(3, 'C', 'East_Campus_Dorms', 803, 800, 1)
-active_list.append(m)
-Map.map[803, 800] = 1
-
-shuffle = [0,1,2,3]
-# shuffle = [0]
-total_so_far = 4
-total_active = 4
-# total_active = 1
-time = 0.5
 
 def validneighbors(position, matrix):
 	(x,y) = position
@@ -67,32 +51,23 @@ def move(people_list, k, time):
 		(x,y) = (people_list[k].x, people_list[k].y)
 
 		if floorfield[x,y] <= 9:			# if the person is at the gate, then make him disappear
-			Map.map[x,y] = 0.0				# clear the previous cell in the map
+			map.map[x,y] = 0.0				# clear the previous cell in the map
 			people_list[k] = people_list[k]._replace(status = 0)	# clear the person's position and make him inactive
 			total_active = total_active - 1									# reduce one active person
 			print "person number", k, "has arrived at", people_list[k].destin, "at time", time
 
 		else:							# if the person is in the middle of the map
 			infinity = float("inf")
-			P = validneighbors((x,y), Map.map)
+			P = validneighbors((x,y), map.map)
 			if len(P) > 0: # if the people_list[k] can move
-				# M = []
-				# for i in range(len(P)):
-					# R = floorfield[P[i]]  # distance between the possible cell and the cloest block of marta gate to the people_list[k]
-				# 	M.append(1.0 / R)
-
-				# rand = random.random()	# random number from uniform distribution
-				# N = 1.0 / np.sum(M)		# normalization constant
-				# m_sum = 0.0				# cumulative probability
-				# for i in range(len(M)):
-				# 	M[i] = M[i] * N 						# normalize probability of moving to P[i]
-				# 	m_sum = m_sum + M[i]					# get cumulative probability of moving to P[i]
-				# 	if rand < m_sum:						# once have find the P[i], break out of the loop
-				# 		move_target = P[i]
-				# 		break
 
 				if len(P)==1:
-					move_target = P[0]
+					temp_target = P[0]
+					if floorfield[temp_target] <= floorfield[x,y]:
+						move_target = temp_target
+					else:
+						move_target = (x,y)
+						
 				elif len(P)>1:
 					D = [floorfield[P[i]] for i in range(len(P))]
 					temp_target = [p for (d,p) in sorted(zip(D,P), key=lambda pair: pair[0])][0]
@@ -101,8 +76,8 @@ def move(people_list, k, time):
 					else:
 						move_target = (x,y)
 
-				Map.map[people_list[k].x, people_list[k].y] = 0.0				# clear the previous cell in the map
-				Map.map[move_target[0], move_target[1]] = 1.0	# block the newly occupied cell
+				map.map[people_list[k].x, people_list[k].y] = 0.0				# clear the previous cell in the map
+				map.map[move_target[0], move_target[1]] = 1.0	# block the newly occupied cell
 				people_list[k] = people_list[k]._replace(x=move_target[0], y=move_target[1])
 
 
@@ -115,18 +90,16 @@ def waitforcw(people_list, k, closedcws, cwstart, cwend):
 			startloc = cwstart[cw]
 			endloc = cwend[cw]
 			cwstartvalue = floorfield[startloc]
-			# print 'cwstartvalue is', cwstartvalue
 			cwendvalue = floorfield[endloc]
-			# print 'endloc is', endloc
-			# print 'cwendvalue is', cwendvalue
+
 
 			if cwstartvalue - cwendvalue > 5:
-				if floorfield[x,y] > cwstartvalue  and floorfield[x,y] < cwstartvalue+5:
+				if floorfield[x,y] > cwstartvalue  and floorfield[x,y] < cwstartvalue+10:
 					people_list[k] = people_list[k]._replace(status = 2)
 					print 'person', k, 'ran into a red light at', (x,y)
 					break
 			elif cwendvalue - cwstartvalue > 5:
-				if floorfield[x,y] > cwendvalue  and floorfield[x,y] < cwendvalue+5:
+				if floorfield[x,y] > cwendvalue  and floorfield[x,y] < cwendvalue+10:
 					people_list[k] = people_list[k]._replace(status = 2)
 					print 'person', k, 'ran into a red light at', (x,y)
 					break
@@ -139,31 +112,35 @@ def greenlight(people_list, k):
 
 cwclosetime = 0.0
 cwopentime = 0.0
+active_list = queue.people_gen(0)
+total_so_far = len(active_list)
+total_active = total_so_far
+shuffle = range(total_so_far)
+time = 0.5
 while total_active != 0:
-	if time%240 == 0:
+	print time
+	if time%cw_close_period == 0:
 		cwclosetime = time
-		cwopentime = cwclosetime + 60
-		closedcws = random.sample(Map.Crosswalks, 3)
+		cwopentime = cwclosetime + cw_close_time
+		closedcws = random.sample(map.Crosswalks, 3)
 
 	random.shuffle(shuffle)
 	for person in shuffle:
 		if time < cwopentime and time >= cwclosetime:
-			waitforcw(active_list, person, closedcws, Map.cwstart, Map.cwend)
+			waitforcw(active_list, person, closedcws, map.cwstart, map.cwend)
 		elif time == cwopentime:
 			greenlight(active_list, person)
 		move(active_list, person, time)
-	# if time < 10:
-	# 	for place in birth:
-	# 		if map.map[place[0], place[1]] == 0:
-	# 			total_so_far = total_so_far + 1
-	# 			total_active = total_active + 1
-	# 			m = personStruct(total_so_far, place[0], place[1], 1)
-	# 			active_list.append(m)
-	# 			shuffle.append(total_so_far-1)
+	if time < 100:
+		new_active = queue.people_gen(total_so_far)
+		active_list.extend(new_active)
+		shuffle.extend(range(total_so_far, total_so_far+len(new_active)))
+		total_so_far = total_so_far + len(new_active)
+		total_active = total_active + len(new_active)
+
 	time = time + 0.5
-	# print total_active
-# print "time is", time
-# print "total number of people simulated is", total_so_far
+
+print "total number of people simulated is", total_so_far
 
 
 
