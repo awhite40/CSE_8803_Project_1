453e2af6970e1254658da3084f981159b830f965
sumana2016 <sumsen.k@gmail.com>
2016-02-25 09:55:42 -0500

    Added files via upload

--- queue.py
+++ queue.py
@@ -20,6 +20,7 @@ def people_gen():
             start = 800
             x=0
             y=0
+            status = 1
             total_people = gate_count * people_per_gate
             global total_active
             personStruct = namedtuple("personStruct", "index gate destin x y status")  # create struct for each person, with index, destination, position,active(1)or not(0)
@@ -29,7 +30,7 @@ def people_gen():
                     x = y = 0
                     gate_range = random.random() 
                     if gate_range >= 0 and gate_range < 0.2:
-                        gate_name = 'Gate A'
+                        gate_name = 'Gate_A'
                         grid_range = random.random() 
                         """
                         if grid_range >=0 and grid_range < (1/16):
@@ -50,7 +51,7 @@ def people_gen():
                                     y = (start - 109 + j+1)    
                         
                     elif gate_range >=0.2 and gate_range < 0.4:
-                        gate_name = 'Gate B'
+                        gate_name = 'Gate_B'
                         grid_range_B = random.random() 
                         for jb in range(16):
                                 x = (470) #Need to verify
@@ -60,7 +61,7 @@ def people_gen():
                                 if grid_range_B >=(jb/16.0) and grid_range_B < ((jb+1)/16.0):
                                     y = (start - 109 + jb+1)  #Need to verify
                     elif gate_range >=0.4 and gate_range < 0.6:
-                        gate_name = 'Gate C'
+                        gate_name = 'Gate_C'
                         grid_range_C = random.random() 
                         for jc in range(16):
                                 x = (796) #Need to verify
@@ -70,7 +71,7 @@ def people_gen():
                                 if grid_range_C >=(jc/16.0) and grid_range_C < ((jc+1)/16.0):
                                     y = (758+jc)  #Need to verify
                     elif gate_range >=0.6 and gate_range < 0.8:
-                        gate_name = 'Gate D'
+                        gate_name = 'Gate_D'
                         grid_range_D = random.random() 
                         for jd in range(16):
                                 x = (start + 81 -457) #Need to verify
@@ -80,7 +81,7 @@ def people_gen():
                                 if grid_range_D >=(jd/16.0) and grid_range_D < ((jd+1)/16.0):
                                     y = (200+jd)  #Need to verify -check
                     elif gate_range >=0.8 and gate_range < 1.0:
-                        gate_name = 'Gate E'
+                        gate_name = 'Gate_E'
                         grid_range_E = random.random() 
                         for je in range(16):
                                 x = (start + 112) #Need to verify
@@ -97,18 +98,18 @@ def people_gen():
                     elif dest_range >=0.14 and dest_range < 0.28:
                         destination = 'Varsity'
                     elif dest_range >=0.28 and dest_range < 0.42:
-                        destination = 'North Ave'
+                        destination = 'North_Ave'
                     elif dest_range >=0.42 and dest_range < 0.56:
-                        destination = 'Peters Parking Deck'
+                        destination = 'Peters_Parking_Deck'
                     elif dest_range >=0.56 and dest_range < 0.7:
-                        destination = 'East Campus Dorms'
+                        destination = 'East_Campus_Dorms'
                     elif dest_range >=0.7 and dest_range < 0.84:
-                        destination = 'Bus Stop'
+                        destination = 'Bus_Stop'
                     elif dest_range >=0.84 and dest_range < 1.00:
-                        destination = 'Student Center'
+                        destination = 'Student_Center'
                         
                     m = personStruct(i,gate_name,destination,800, 1557, 1)    # create person 0
-                    print "i is: "+str(i) + " gate_name is:"+ str(gate_name) + " destination is:"+ str(destination) + " x is:" +str(x) + " y is:" +str(y)
+                    print "i is: "+str(i) + " gate_name is:"+ str(gate_name) + " destination is:"+ str(destination) + " x is:" +str(x) + " y is:" +str(y) + "status is: " +str(1)
 
                     #determine x and y
                         
